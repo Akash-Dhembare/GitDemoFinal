53b2075c25358595d1197c26926038da710864b8
Akash <akash.dhembare777@gmail.com>
2025-06-15 13:03:00 +0530

    This is commit from Indian User

---
+++ JavaOOPS/src/main/java/com/akash/dhembare2000/SampleFile_GitDemo_1.java
@@ -0,0 +1,7 @@
+package com.akash.dhembare2000;
+
+public class SampleFile_GitDemo_1 {
+    public static void main(String[] args) {
+        System.out.println("Sample file 1 for Git Demo");
+    }
+}
