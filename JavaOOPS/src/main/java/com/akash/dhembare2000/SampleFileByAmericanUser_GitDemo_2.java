d9b24c25342e9f2d06feb6ec0bc76ff96850c384
Akash <akash.dhembare777@gmail.com>
2025-06-15 13:22:37 +0530

    Changes by American User

---
+++ JavaOOPS/src/main/java/com/akash/dhembare2000/SampleFileByAmericanUser_GitDemo_2.java
@@ -0,0 +1,7 @@
+package com.akash.dhembare2000;
+
+public class SampleFileByAmericanUser_GitDemo_2 {
+    public static void main(String[] args) {
+        System.out.println("Newly created file by American User");
+    }
+}
--- JavaOOPS/src/main/java/com/akash/dhembare2000/SampleFile_GitDemo_1.java
+++ JavaOOPS/src/main/java/com/akash/dhembare2000/SampleFile_GitDemo_1.java
@@ -4,4 +4,8 @@ public class SampleFile_GitDemo_1 {
     public static void main(String[] args) {
         System.out.println("Sample file 1 for Git Demo");
     }
+
+    public void ChangesByAmericanUser(){
+        System.out.println("Changes done by american user");
+    }
 }
